2f76f76c0a2f7f796425eed6043f40f0c4c947cb
Ma Chi <hellomarch@gmail.com>
2024-03-01 12:52:39 +0100

    Wait until app is up

--- localhost/install.sh
+++ localhost/install.sh
@@ -26,14 +26,23 @@ cd -
 # Create a kubernetes deployment and a service for the app
 kubectl apply -f bullets-app.yaml
 
+# Loop wait until the app is running
+while [ $(kubectl get pods | grep bullets-app | grep Running | wc -l | tr -d ' ') -eq 0 ]; do
+  echo "Waiting for the app to be running"
+  sleep 5
+done
+
 # forward the port to the app
 mkdir ./log
 kubectl port-forward service/bullets-app-service 8080:8080 > ./log/port-forward.log 2>&1 &
+
 
 # Run the client in a docker container
 cd ../test/e2e
 docker build -t bullets-client:latest .
 cd -
+
+
 
 docker rm -f client
 docker run -d --env "LB_ADDRESS=host.docker.internal" --env "CONCURRENCY=100" --name client bullets-client
