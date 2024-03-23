587855befa513e2e34563139f0c7aa3f88fa3dac
Ma Chi <hellomarch@gmail.com>
2024-03-23 18:54:08 +0100

    rename the cluster

--- infrastructure/service/provider.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "lipingtababa-tf-statefiles"
-    key    = "bullets/service/terraform.tfstate"
-    region = "us-east-1"
-    dynamodb_table = "lipingtababa-terraform-lock-table"
-  }
-}
